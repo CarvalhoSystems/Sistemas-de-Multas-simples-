0af1fca890bfbeaccae7d2b0243908e24e33d0f4
CarvalhoSystems <carvalho_borges@icloud.com>
2025-09-06 17:46:28 -0300

    att
    mudanças no layout.

--- src/main.js
+++ src/main.js
@@ -2,21 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
    const velocidade = document.querySelector('#vel');
    const resultado = document.querySelector('#resultado');
    const botao = document.querySelector('#verificar');
+   const vias = document.querySelector('#vias'); // select das vias
    
    botao.addEventListener("click", () => {
-    const vel = Number(velocidade.value);
-    resultado.innerHTML = `Sua velocidade foi de ${vel} km/h`
-    
+      const vel = Number(velocidade.value);
 
-   if (!velocidade.value) {
-    resultado.innerHTML = "DIgite sua Velocidade primeiro";
-    return;
-   }
-   if(vel > 60){
-    resultado.innerHTML = `Sua Velocidade é de <stong>${vel} km/h</strong>.Você Foi <strong>multado</strong>`;
-      }else {
-        resultado.innerHTML = `Sua Velocidade é de <strong> ${vel} km/h</strong>.Você está dentro do limite`;
+      if (!velocidade.value.trim()) {
+         resultado.innerHTML = "Digite sua Velocidade primeiro";
+         return;
+      }
+
+      resultado.innerHTML = `Sua velocidade foi de ${vel} km/h`;
+
+      if (vias.value === "avenida" && vel > 60) {
+         resultado.innerHTML = `Sua Velocidade é de <strong>${vel} km/h</strong>. Você Foi <strong>multado</strong>`;
+      } else if (vias.value === "Rodovia" && vel > 120) {
+         resultado.innerHTML = `Sua Velocidade é de <strong>${vel} km/h</strong>. Você Foi <strong>multado</strong>`;
+      } else {
+         resultado.innerHTML = `Sua Velocidade é de <strong>${vel} km/h</strong>. Você está dentro do limite`;
       }
    });
 });
-
